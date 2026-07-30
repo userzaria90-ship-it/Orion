289fbaff0ac79bd33eab634e91c855120983422d
Rdb. <user.zaria90@gmail.com>
2026-07-30 18:17:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
 module.exports = {
 
-    staffRole: "1506327079510081667",
-
-    categories: {
-        entreprise: "1506327182253752402",
-        organisation: "1506414004174717089",
-        boutique: "1506620732166115410",
-        deban: "1506619767417737288",
-        remboursement: "1506622010170806398",
-        plainteJoueur: "1506619638786555965",
-        plainteStaff: "1506619466929279207",
-        autre: "1506327181700370513"
-    }
-
-}
+    staffRole: "1532417726662836357",
