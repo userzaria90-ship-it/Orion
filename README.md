36e6db7ac66c15eeeec69eba08cae5cba712042e
Rdb. <user.zaria90@gmail.com>
2026-07-30 18:12:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Support-bot-V2
+First bot By rdb dm for keys
---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    staffRole: "1506327079510081667",
+
+    categories: {
+        entreprise: "1506327182253752402",
+        organisation: "1506414004174717089",
+        boutique: "1506620732166115410",
+        deban: "1506619767417737288",
+        remboursement: "1506622010170806398",
+        plainteJoueur: "1506619638786555965",
+        plainteStaff: "1506619466929279207",
+        autre: "1506327181700370513"
+    }
+
+}
